3f9b09ed0ea93a1619bc83a5119d9160be056db2
mishalturkane <mishalturkane@gmail.com>
2026-08-05 22:02:07 +0530

    feat: Result error demo added

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,7 +1,10 @@
-pub fn panic_on_condition(x: i32) {
-    if x < 0 {
-        panic!("x cannot be negative, got: {}", x);
+pub fn divide<T>(a: T, b: T) -> Result<T, String>
+where
+    T: std::ops::Div<Output = T> + PartialEq + Default,
+{
+    if b == T::default() {   // T::default() = 0 for i32, 0.0 for f64
+        Err(String::from("denominator cannot be zero"))
+    } else {
+        Ok(a / b)
     }
-    println!("x is: {}", x);
 }
-
