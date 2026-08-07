162f2d26e1f8df84b21ea52736f71bdf93a9a024
mishalturkane <mishalturkane@gmail.com>
2026-08-07 08:06:36 +0530

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 #![allow(dead_code)]
 
-#[path = "enums-pattern_matching/mod.rs"]
-mod enums_pattern_matching;
-mod errors;
 #[path = "iterators_clousers/mod.rs"]
 mod iterators_clousers;
 
 fn main() {
-    // ===== ENUMS =====
-    println!("\n===== ENUMS 1. IF LET =====");
-    enums_pattern_matching::demo_if_let();
-
-    println!("\n===== ENUMS 2. DESTRUCTURING =====");
-    enums_pattern_matching::demo_destructuring();
-
-    println!("\n===== ENUMS 3. STATE MACHINE =====");
-    enums_pattern_matching::demo_state_machine();
-
-    println!("\n===== ENUMS 4. RESULT HANDLING =====");
-    enums_pattern_matching::demo_result_handling();
-
-    println!("\n===== ENUMS 5. API DESIGN =====");
-    enums_pattern_matching::demo_api_design();
-
+   
     // ===== ITERATORS & CLOSURES =====
     println!("\n===== ITER 1.  CLOSURE BASICS =====");
     iterators_clousers::demo_closure_basics();
